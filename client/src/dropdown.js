750822150548dea578ee96866e7a7c67852b5c58
mansi22 <mansithakkar9990@gmail.com>
2022-01-10 11:42:47 +0530

    Add files via upload

---
+++ client/src/dropdown.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Select from 'react-select';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const Countries = [
+    { label: "New Delhi", value: "New Delhi" },
+    { label: "Sydney", value: "Sydney" },
+    { label: "New York", value: "New York" },
+    { label: "London", value: "London" },
+    { label: "Kuwait", value: "Kuwait" },
+    { label: "Pune", value: "Pune" },
+    { label: "Jaipur", value: "Jaipur" }
+  ];
+  export default class SelectDropDownComponent extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            city:""
+        }
+    }
+    render() {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3"></div>
+            <div className="col-md-6">
+              <Select options={Countries} defaultValue={{ label: "Select City", value: 0 }} onChange={e => {
+              this.setState({
+              city:e.value
+              });
+           }} />
+            </div>
+            <div className="col-md-4"></div>
+          </div>
+        </div>
+      );
+    }
+  
+  }
